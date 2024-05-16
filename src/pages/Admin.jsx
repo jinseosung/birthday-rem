1f6f9e8f3a7157852bac58d303658e4022dd0b66
Jinseo Sung <jinseosung15@gmail.com>
2024-05-16 15:17:46 +0200

    add search bar

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -3,8 +3,12 @@ import React, { useEffect, useState } from "react";
 import { collection, query, onSnapshot } from "firebase/firestore";
 import { db } from "../firebase";
 
+import quizAnswer from "../datas/quiz.json";
+
 function Admin() {
   const [results, setResults] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const correctAnswer = quizAnswer.map((quiz) => quiz.correctAnswer);
 
   const q = query(collection(db, "score"));
   const fetchResults = () => {
@@ -20,23 +24,47 @@ function Admin() {
 
   useEffect(() => {
     fetchResults();
-  }, []);
+  });
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredResults = results.filter((result) =>
+    result.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const renderResult = (result, index) => (
+    <div key={index} className="btnTransparent">
+      <div className="top">
+        <div>{result.name}</div>
+        <div>{result.score}</div>
+      </div>
+      <ul className="result-ul">
+        {result.answerList.map((answer, answerIndex) => (
+          <li
+            key={answerIndex}
+            className={
+              answer !== correctAnswer[answerIndex] ? "incorrect-answer" : ""
+            }
+          >{`${answerIndex + 1} : ${answer}`}</li>
+        ))}
+      </ul>
+    </div>
+  );
 
   return (
     <div className="result-container">
-      {results.map((result, index) => (
-        <div key={index} className="btnTransparent">
-          <div className="top">
-            <div>{result.name}</div>
-            <div>{result.score}</div>
-          </div>
-          <ul className="result-ul">
-            {result.answerList.map((answer, index) => (
-              <li key={index}>{`${index + 1} : ${answer}`}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      <input
+        className="result-input"
+        type="text"
+        value={searchTerm}
+        onChange={handleSearch}
+        placeholder="Trouvez par votre nom"
+      />
+      {searchTerm === ""
+        ? results.map(renderResult)
+        : filteredResults.map(renderResult)}
     </div>
   );
 }
